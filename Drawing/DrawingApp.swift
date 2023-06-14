09c667cc312d28cdc968f96a1fd5be87f654033a
RishavG96 <gupta.rishav2608@gmail.com>
2023-06-14 07:47:37 +0530

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Rishav Gupta on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
